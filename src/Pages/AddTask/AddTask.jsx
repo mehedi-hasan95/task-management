a379e21721a57f423f4dc58c2a641e64e7c55655
mehedititu <mh.me.mehedi@gmail.com>
2022-12-29 00:51:42 +0600

    Send Image to imgbb

--- src/Pages/AddTask/AddTask.jsx
+++ src/Pages/AddTask/AddTask.jsx
@@ -1,19 +1,46 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../../Components/AuthProvider/AuthProvider";
 
 const AddTask = () => {
     const [file, setFile] = useState(null);
+    const { register, handleSubmit } = useForm();
+    const { user } = useContext(AuthContext);
+
+    const uploadImage = process.env.REACT_APP_image_host;
+
+    const submitTast = (data) => {
+        const img = data.img[0];
+        const formData = new FormData();
+        formData.append("image", img);
+        const url = `https://api.imgbb.com/1/upload?key=${uploadImage}`;
+        fetch(url, {
+            method: "POST",
+            body: formData,
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                console.log("Success:", result);
+            });
+    };
 
     return (
         <div className="p-6 dark:bg-gray-800 dark:text-gray-50">
-            <form className=" max-w-3xl flex flex-col mx-auto space-y-12 ng-untouched ng-pristine ng-valid">
+            <form
+                onSubmit={handleSubmit(submitTast)}
+                className=" max-w-3xl flex flex-col mx-auto space-y-12 ng-untouched ng-pristine ng-valid"
+            >
                 <div className="col-span-full">
                     <label htmlFor="title" className="">
                         Task Name
                     </label>
                     <input
-                        type="text"
+                        {...register("title", {
+                            required: "Product name is Required",
+                        })}
+                        type="name"
                         name="title"
-                        id="title"
+                        placeholder="Product Name"
                         className="w-full rounded-md px-3 py-2 border focus:ring focus:ring-opacity-75 focus:ring-violet-400 outline-none dark:text-gray-900"
                     />
                 </div>
@@ -22,10 +49,12 @@ const AddTask = () => {
                         Task Details
                     </label>
                     <textarea
-                        name="description"
-                        id="description"
-                        rows="12"
+                        {...register("desc", {
+                            required: "Description is Required",
+                        })}
+                        name="desc"
                         className="w-full rounded-md px-3 py-2 border focus:ring focus:ring-opacity-75 focus:ring-violet-400 outline-none dark:text-gray-900"
+                        rows="12"
                     ></textarea>
                 </div>
                 <div className="col-span-full">
@@ -44,16 +73,18 @@ const AddTask = () => {
                     )}
                     <fieldset className="w-full space-y-1 dark:text-gray-100 pt-5">
                         <label
-                            for="files"
+                            htmlFor="files"
                             className="block text-sm font-medium"
                         >
                             Add Thumbanil
                         </label>
                         <div className="flex">
                             <input
+                                {...register("img", {
+                                    required: "Image is Required",
+                                })}
                                 type="file"
-                                name="files"
-                                id="files"
+                                name="img"
                                 className="px-8 py-12 border-2 border-dashed rounded-md dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800"
                                 onChange={(e) => setFile(e.target.files[0])}
                             />
--- src/Pages/Home/SiteInfo/SiteInfo.jsx
+++ src/Pages/Home/SiteInfo/SiteInfo.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import { GoogleAuthProvider } from "firebase/auth";
+import React, { useContext } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../Components/AuthProvider/AuthProvider";
 
 const SiteInfo = () => {
+    const { googleLogin } = useContext(AuthContext);
+
+    // Login With Google
+    const provider = new GoogleAuthProvider();
+    const handleGoogle = () => {
+        googleLogin(provider)
+            .then((result) => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch((error) => console.error(error));
+    };
     return (
         <div className="pt-7 dark:bg-gray-900 py-5">
             <h2 className="text-center text-gray-500 text-3xl md:text-5xl">
@@ -14,7 +28,10 @@ const SiteInfo = () => {
                     Aligned.
                 </span>
             </h2>
-            <button className="flex items-center text-2xl md:text-3xl text-white bg-[#6445AC] px-5 py-2 rounded-full mx-auto mt-7 hover:shadow-lg hover:shadow-[#8146AD]">
+            <button
+                onClick={handleGoogle}
+                className="flex items-center text-2xl md:text-3xl text-white bg-[#6445AC] px-5 py-2 rounded-full mx-auto mt-7 hover:shadow-lg hover:shadow-[#8146AD]"
+            >
                 <FcGoogle className="pr-5 text-5xl md:text-6xl" />
                 Continue with Google
             </button>
